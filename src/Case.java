bde8302729ef2d83dd323f3e4d7b5af105a3d98f
Josiah Parrott <josiahparrott@gmail.com>
2023-10-06 14:02:55 -0400

    Initial Commit

---
+++ src/Case.java
@@ -0,0 +1,90 @@
+/**
+ * Case class that has a number,
+ * an amount, and the ability to
+ * determine if the case has already
+ * been picked.
+ */
+
+public class Case {
+    int num;
+    int amount;
+    boolean canBPicked;
+
+    /**
+     * An initializing constructor
+     */
+    public Case() {
+        this.num = 0;
+        this.amount = 0;
+        this.canBPicked = false;
+    }
+
+    /**
+     * A constructor that sets the number and
+     * the amount
+     *
+     * @param num   Number of the case
+     * @param amount    Amount of the case
+     */
+    public Case(int num, int amount) {
+        this.num = num;
+        this.amount = amount;
+        this.canBPicked = true;
+    }
+
+    /**
+     * Sets the number of a case
+     *
+     * @param num   Number to set
+     */
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    /**
+     * Sets the amount of a case
+     *
+     * @param amount    Amount to set
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * Sets if the case can be picked
+     *
+     * @param canBPicked    Can it be picked
+     */
+    public void setCanBPicked(boolean canBPicked) {
+        this.canBPicked = canBPicked;
+    }
+
+    /**
+     * Gets a cases number
+     *
+     * @return  Number of case
+     */
+    public int getNum() {
+        return num;
+    }
+
+    /**
+     * Gets a cases amount
+     *
+     * @return  Amount of case
+     */
+    public int getAmount() {
+        return amount;
+    }
+
+    /**
+     * Sends case to string
+     *
+     * @return  String
+     */
+    public String toString() {
+        return "Case number " + num +
+               " has $" + amount +
+               "!";
+    }
+}
